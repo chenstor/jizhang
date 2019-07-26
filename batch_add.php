4895bf4e19bf3031664231d116b14caf895c2275
chenstor <chenstor@qq.com>
2019-07-26 16:20:03 +0800

    优化页面点击动作

--- batch_add.php
+++ batch_add.php
@@ -47,7 +47,7 @@ if (isset($_POST['submit'])){
 }
 ?>
 <table align="left" width="100%" border="0" cellpadding="5" cellspacing="1" bgcolor='#B3B3B3' class='table table-striped table-bordered'>
-<tr><td bgcolor="#EBEBEB"><span class="red">支出</span> - <a href="#" class="AddBox" date-info="2">加一行</a></td></tr>
+<tr><td bgcolor="#EBEBEB"><span class="red">支出</span> - <a href="javascript:void(0);" class="AddBox" date-info="2">加一行</a></td></tr>
 <tr><td bgcolor="#FFFFFF">
 	<form id="form_2" name="form_2" method="post">
 		<div id="itlu_wrap_2" class="itlu_wrap">
@@ -65,7 +65,7 @@ if (isset($_POST['submit'])){
 		<div class="sub_btn"><input name="submit" type="submit" id="submit" value="支出记账" class="btn btn-danger" /></div>
 	</form>
 </td></tr>
-<tr><td bgcolor="#EBEBEB"><span class="green">收入</font> - <a href="#" class="AddBox" date-info="1">加一行</a></td></tr>
+<tr><td bgcolor="#EBEBEB"><span class="green">收入</font> - <a href="javascript:void(0);" class="AddBox" date-info="1">加一行</a></td></tr>
 <tr><td bgcolor="#FFFFFF">
 	<form id="form_1" name="form_1" method="post">
 		<div id="itlu_wrap_1" class="itlu_wrap">
@@ -93,7 +93,7 @@ $("#form_1").keypress(function(e){
 });
 $(document).ready(function(){
 	var MaxInputs = 4;		
-	var html = "<div class=\"list\"><input name=\"zhifu[]\" type=\"hidden\" value=\"2\" /><div class=\"list_1\"><i>金额：</i><input type=\"text\" name=\"money[]\" /></div><div class=\"list_2\"><i>分类：</i><select name=\"classid[]\"><?php echo $pay_type_option;?></select></div><div class=\"list_3\"><i>备注：</i><input name=\"remark[]\" type=\"text\" /></div><div class=\"list_4\">时间：<input type=\"text\" name=\"time[]\" value=\"<?php echo date("Y-m-d H:i");?>\" onclick=\"WdatePicker({dateFmt:'yyyy-MM-dd HH:mm',maxDate:'<?php echo $today;?>'})\" /></div><div class=\"list_5\"><a class=\"removeclass\" href=\"#\">删除</a></div></div>";
+	var html = "<div class=\"list\"><input name=\"zhifu[]\" type=\"hidden\" value=\"2\" /><div class=\"list_1\"><i>金额：</i><input type=\"text\" name=\"money[]\" /></div><div class=\"list_2\"><i>分类：</i><select name=\"classid[]\"><?php echo $pay_type_option;?></select></div><div class=\"list_3\"><i>备注：</i><input name=\"remark[]\" type=\"text\" /></div><div class=\"list_4\">时间：<input type=\"text\" name=\"time[]\" value=\"<?php echo date("Y-m-d H:i");?>\" onclick=\"WdatePicker({dateFmt:'yyyy-MM-dd HH:mm',maxDate:'<?php echo $today;?>'})\" /></div><div class=\"list_5\"><a class=\"removeclass\" href=\"javascript:void(0);\">删除</a></div></div>";
 	var x = 1;
 	var FieldCount = 1;
 	$(".AddBox").click(function(e){
